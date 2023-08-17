b976a8481bcf78c54d632990f0883ab8ea76fcfb
Peng-LUH <ethaniel.unihannover@gmail.com>
2023-08-17 14:30:21 +0200

    update

--- run.py
+++ run.py
@@ -6,8 +6,6 @@ from flask import jsonify
 # sys.path.append(os.getcwd())
 # sys.path.append("/home/rathee/search/aims/search_l3s_aimeta_srv/src")
 
-
-
 from src.search_l3s_aimeta import create_app, db
 
 app = create_app(os.getenv("FLASK_ENV", "development"))
