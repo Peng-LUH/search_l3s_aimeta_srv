416e3137b675c25574c0d4aff6f7b3fe60c95576
Mandeep-Rathee <mandeeprathee77@gmail.com>
2023-08-17 14:06:15 +0200

    modified:   run.py

--- run.py
+++ run.py
@@ -2,6 +2,11 @@
 import os
 from flask import jsonify
 
+# import sys
+# sys.path.append(os.getcwd())
+# sys.path.append("/home/rathee/search/aims/search_l3s_aimeta_srv/src")
+
+
 
 from src.search_l3s_aimeta import create_app, db
 
