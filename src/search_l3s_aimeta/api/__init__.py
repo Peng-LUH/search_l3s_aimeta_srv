7fd86c42ce83fb5d78a6a0d6f4e5e0851aa82998
Mandeep-Rathee <mandeeprathee77@gmail.com>
2023-11-08 17:51:21 +0100

    updated version

--- src/search_l3s_aimeta/api/__init__.py
+++ src/search_l3s_aimeta/api/__init__.py
@@ -12,7 +12,7 @@ api_bp = Blueprint("api", __name__, url_prefix="/l3s-aimeta")
 
 
 api = Api(api_bp,
-          version="0.0.1",
+          version="0.0.2",
           title="L3S AI-Meta Service(AIMS) for SEARCH",
           description="Welcome to the Swagger UI documentation site!",
           # doc="/ui",
