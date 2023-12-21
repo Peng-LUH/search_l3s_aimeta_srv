0bab8f25a6188015b84e459538c2a5f174cdb8ee
Mandeep-Rathee <mandeeprathee77@gmail.com>
2023-12-21 10:17:10 +0100

    update

--- src/search_l3s_aimeta/api/dataset_preprocess/dto.py
+++ src/search_l3s_aimeta/api/dataset_preprocess/dto.py
@@ -5,7 +5,7 @@ dto_task_preprocess_response = Model("DtoTaskPreprocessResponse",
                                         {
                                         "task_id" : fields.String(description='The task ID', example='10'),
                                         "task_title": fields.String(description='Title of the task', example='Schlitten [3_3]'),
-                                        "text": fields.String(description='Processed text of the task', example='1. informieren Schlitten:  Arbeitsauftrag Sind deine Ziele ...  Lernfortschritt besser erkennen kannst.'),
+                                        "text": fields.String(description='Processed text of the task', example="1. informieren Schlitten Arbeitsauftrag Sind deine Ziele...Lernfortschritt besser erkennen kannst."),
                                         "tasksteps_ids": fields.List(fields.Integer, description="taskstep ids in the task", example= [23,24,25,26,27,28, 29]),
                                         "task_set_id" : fields.String(description="id of task set, corresponding to different owner/category", example= "2")
                                         }) 
@@ -13,5 +13,5 @@ dto_task_preprocess_response = Model("DtoTaskPreprocessResponse",
 dto_taskstep_preprocess_response = Model("DtoTaskStepPreprocessResponse",
                                         {
                                         "taskstep_id" : fields.String(description='The task step ID', example='23'),
-                                        "taskstep_text": fields.String(description='Processed text of the task step', example='Arbeitsauftrag Sind deine Ziele festgelegt? ...  Deine persönliche Reflexion bitte in Notizen ablegen:'),
+                                        "taskstep_text": fields.String(description='Processed text of the task step', example="Arbeitsauftrag Sind deine Ziele festgelegt?...Deine persönliche Reflexion bitte in Notizen ablegen"),
                                         }) 
