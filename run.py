92a338b6a11c84b51ecbc60e4ba2a3b8252f0fea
Mandeep-Rathee <mandeeprathee77@gmail.com>
2024-06-04 13:50:55 +0200

    new branch init

--- run.py
+++ run.py
@@ -11,7 +11,7 @@ from src.search_l3s_aimeta import create_app, db
 app = create_app(os.getenv("FLASK_ENV", "development"))
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port="9041")
+    app.run(debug=True, host="0.0.0.0")
 
 @app.shell_context_processor
 def shell():
